eabefcd112fc9dc143944743c8c5599462b22fcc
Cassio Santana <cassioo.roger@gmail.com>
2023-04-01 15:38:36 -0300

    refactor(search): modified linear search test

--- spec/search/linear_spec.rb
+++ spec/search/linear_spec.rb
@@ -1,41 +1,33 @@
 require 'search/linear'
 
 RSpec.describe Linear do
-  it 'element found' do
-    arr = [5, 7, 10, 15, 18, 27]
-    target_element = 18
+  context 'searching for element in an array' do
+    it 'element found' do
+      arr = [5, 7, 10, 15, 18, 27]
+      target_element = 18
+      result = Linear.new.search(arr, target_element)
+      expect(result).to be >= 0
+    end
 
-    result = Linear.new.search(arr, target_element)
-    expect(result).to be >= 0
-  end
-end
+    it 'element found at index 4' do
+      arr = [5, 7, 10, 15, 18, 27]
+      target_element = 18
+      result = Linear.new.search(arr, target_element)
+      expect(result).to eq(4)
+    end
 
-RSpec.describe Linear do
-  it 'element found at index 4' do
-    arr = [5, 7, 10, 15, 18, 27]
-    target_element = 18
+    it 'element not found' do
+      arr = [5, 7, 10, 15, 18, 27]
+      target_element = 200
+      result = Linear.new.search(arr, target_element)
+      expect(result).to eq(-1)
+    end
 
-    result = Linear.new.search(arr, target_element)
-    expect(result).to eql(4)
-  end
-end
-
-RSpec.describe Linear do
-  it 'element not found' do
-    arr = [5, 7, 10, 15, 18, 27]
-    target_element = 200
-
-    result = Linear.new.search(arr, target_element)
-    expect(result).to eql(-1)
-  end
-end
-
-RSpec.describe Linear do
-  it 'empty list' do
-    arr = []
-    target_element = 5
-
-    result = Linear.new.search(arr, target_element)
-    expect(result).to eql(-1)
+    it 'empty list' do
+      arr = []
+      target_element = 5
+      result = Linear.new.search(arr, target_element)
+      expect(result).to eq(-1)
+    end
   end
 end
