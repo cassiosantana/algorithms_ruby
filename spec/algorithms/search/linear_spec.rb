d759a5b621421d6649f5e64183075ed5385a93d0
Cassio Santana <cassioo.roger@gmail.com>
2023-04-03 13:18:43 -0300

    refactor(search): linear search test had a module modified

--- spec/algorithms/search/linear_spec.rb
+++ spec/algorithms/search/linear_spec.rb
@@ -1,32 +1,34 @@
+# frozen_string_literal: true
+
 require 'algorithms/search/linear'
 
-RSpec.describe Linear do
+RSpec.describe Algorithms do
   context 'Linear search' do
     it 'element found' do
       arr = [5, 7, 10, 15, 18, 27]
       target_element = 18
-      result = Linear.new.search(arr, target_element)
+      result = Algorithms::LinearSearch.new.search(arr, target_element)
       expect(result).to be >= 0
     end
 
     it 'element found at index 4' do
       arr = [5, 7, 10, 15, 18, 27]
       target_element = 18
-      result = Linear.new.search(arr, target_element)
+      result = Algorithms::LinearSearch.new.search(arr, target_element)
       expect(result).to eq(4)
     end
 
     it 'element not found' do
       arr = [5, 7, 10, 15, 18, 27]
       target_element = 200
-      result = Linear.new.search(arr, target_element)
+      result = Algorithms::LinearSearch.new.search(arr, target_element)
       expect(result).to eq(-1)
     end
 
     it 'empty list' do
       arr = []
       target_element = 5
-      result = Linear.new.search(arr, target_element)
+      result = Algorithms::LinearSearch.new.search(arr, target_element)
       expect(result).to eq(-1)
     end
   end
