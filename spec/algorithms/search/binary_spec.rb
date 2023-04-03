fe9830302f62d31ae53514403b5de61a57ef28cf
Cassio Santana <cassioo.roger@gmail.com>
2023-04-03 17:12:08 -0300

    test(search): binary search

---
+++ spec/algorithms/search/binary_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'algorithms/search/binary'
+
+RSpec.describe Algorithms do
+  context 'Binary search' do
+    it 'Element found' do
+      arr = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
+      element = 50
+      last = arr.length - 1
+
+      result = Algorithms::BinarySearch.new.search(arr, element, 0, last)
+
+      expect(result).to eq(3)
+    end
+
+    it 'Element not found' do
+      arr = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170]
+      element = 22
+      last = arr.length - 1
+
+      result = Algorithms::BinarySearch.new.search(arr, element, 0, last)
+
+      expect(result).to eq(-1)
+    end
+  end
+end
