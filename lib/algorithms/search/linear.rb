18f499921057b99e1bcc017463069fab10dc815b
Cassio Santana <cassioo.roger@gmail.com>
2023-04-03 13:17:08 -0300

    refactor(search): linear search class added to a module

--- lib/algorithms/search/linear.rb
+++ lib/algorithms/search/linear.rb
@@ -1,8 +1,13 @@
-class Linear
-  def search(arr, target_element)
-    arr.each_with_index do |element, index|
-      return index if element.eql? target_element
+# frozen_string_literal: true
+
+module Algorithms
+  # linear search
+  class LinearSearch
+    def search(arr, target_element)
+      arr.each_with_index do |element, index|
+        return index if element.eql? target_element
+      end
+      -1
     end
-    -1
   end
 end
