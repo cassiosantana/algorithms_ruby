7cc64e5f823b238d52af7887c7e538d66b7d3fc6
Cassio Santana <cassioo.roger@gmail.com>
2023-04-03 17:11:47 -0300

    feat(search): binary search

---
+++ lib/algorithms/search/binary.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Algorithms
+  # binary search
+  class BinarySearch
+    def search(arr, element, first, last)
+      if last >= first
+        mid = (first + last) / 2
+        if element == arr[mid]
+          mid
+        elsif element < arr[mid]
+          search(arr, element, first, mid - 1)
+        else
+          search(arr, element, mid + 1, last)
+        end
+      else
+        -1
+      end
+    end
+  end
+end
