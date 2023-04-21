8eb4bb1e11a4949a786bf4b1454c9826a21822f8
Cassio Santana <cassioo.roger@gmail.com>
2023-04-21 10:16:35 -0300

    build(string): Iniciado count
    Ref #14

---
+++ lib/problems/string/count.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Problems
+  # classe que realiza contagem de caracteres
+  class Count
+    attr_accessor :upper, :lower, :special, :number
+
+    def initialize(str)
+      str.each_char do |char|
+      end
+    end
+  end
+end
