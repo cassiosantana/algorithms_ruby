9d8a46990455b6be913ac6fa9e4c854ad3c7dc8f
Cassio Santana <cassioo.roger@gmail.com>
2023-04-30 15:23:28 -0300

    test(counting_sort): Finalizado teste
    Ref #4

--- spec/algorithms/sorting/counting_spec.rb
+++ spec/algorithms/sorting/counting_spec.rb
@@ -3,7 +3,11 @@
 require 'algorithms/sorting/counting'
 
 RSpec.describe Algorithms do
-  describe 'Bubble Sort' do
-    it ''
+  describe 'Counting Sort' do
+    it 'order positive numbers' do
+      arr = [1, 4, 1, 2, 7, 5, 2]
+      result = Algorithms::CountingSort.order_positive_numbers(arr)
+      expect(result).to eq(arr.sort)
+    end
   end
 end
