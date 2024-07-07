bb3f981748e981e0bee409ee10294e676b6cd03b
Cassio Santana <cassioo.roger@gmail.com>
2024-07-06 23:46:05 -0300

    refactor: bubble sort algorithm
    RF #50

--- lib/algorithms_ruby/bubble_sort.rb
+++ lib/algorithms_ruby/bubble_sort.rb
@@ -2,17 +2,15 @@
 
 module AlgorithmsRuby
   class BubbleSort
-    def sorter(arr)
-      n = arr.length
+    def sort(arr)
+      size = arr.length
 
       loop do
         swapped = false
-        (n - 1).times do |i|
+        (size - 1).times do |i|
           next if arr[i] < arr[i + 1]
 
-          temp = arr[i]
-          arr[i] = arr[i + 1]
-          arr[i + 1] = temp
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
           swapped = true
         end
         break unless swapped
