1d0dcf62461ca5a524fd175f3e04e62e046bf996
Cassio Santana <cassioo.roger@gmail.com>
2024-07-07 14:15:59 -0300

    refactor: binary search algorithm
    RF #50

--- lib/algorithms_ruby/binary_search.rb
+++ lib/algorithms_ruby/binary_search.rb
@@ -3,13 +3,45 @@
 module AlgorithmsRuby
   # binary search
   class BinarySearch
-    def search(arr, element, first, last)
-      return -1 if last < first
 
-      mid = (first + last) / 2
-      return mid if element == arr[mid]
+    def self.run(array, element)
+      new(array, element).search
+    end
 
-      element < arr[mid] ? search(arr, element, first, mid - 1) : search(arr, element, mid + 1, last)
+    def initialize(array, element)
+      @array = array
+      @element = element
+      @first = 0
+      @last = @array.length - 1
+      @mid = nil
+    end
+
+    def search
+      return -1 unless valid_range?
+
+      @mid = calculate_mid
+      return @mid if element_found?
+
+      recursive_search
+    end
+
+    private
+
+    def valid_range?
+      @first <= @last
+    end
+
+    def calculate_mid
+      (@first + @last) / 2
+    end
+
+    def element_found?
+      @element == @array[@mid]
+    end
+
+    def recursive_search
+      @element < @array[@mid] ? @last = @mid - 1 : @first = @mid + 1
+      search
     end
   end
 end
