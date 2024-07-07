06bddc8bf54774833bd5840c24c59f21fbe6517e
Cassio Santana <cassioo.roger@gmail.com>
2024-07-07 14:16:12 -0300

    test: refactor binary search test
    RF #50

--- spec/algorithms_ruby/binary_search_spec.rb
+++ spec/algorithms_ruby/binary_search_spec.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.describe AlgorithmsRuby do
+RSpec.describe AlgorithmsRuby::BinarySearch do
   context "Binary search" do
     let(:arr) { [10, 20, 30, 50, 60, 80, 110, 130, 140, 170] }
-    let(:last) { arr.length - 1 }
-    let(:binary_search) { AlgorithmsRuby::BinarySearch.new }
 
     shared_examples "binary search result" do |element, expected_result|
       it "returns #{expected_result} for element #{element}" do
-        result = binary_search.search(arr, element, 0, last)
+        result = described_class.run(arr, element)
         expect(result).to eq(expected_result)
       end
     end
