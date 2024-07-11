ab8359508e73e145639cc7a25e88be2cde14b0d0
Cassio Santana <cassioo.roger@gmail.com>
2024-07-11 15:23:06 -0300

    test: [wip] refactor find three test
    RF #50

--- spec/algorithms_ruby/find_three_spec.rb
+++ spec/algorithms_ruby/find_three_spec.rb
@@ -1,17 +1,37 @@
 # frozen_string_literal: true
 
-RSpec.describe Arrays::FindTree do
-  describe ".find" do
-    subject { described_class.find(arr) }
+RSpec.describe AlgorithmsRuby::FindThree do
+  describe ".call" do
+    let(:result) { described_class.call(arr) }
 
     context "when array elements are distinct" do
       let(:arr) { [10, 4, 3, 50, 23, 90] }
-      it { expect(subject).to eq(arr.max(3)) }
+      it { expect(result).to eq(arr.max(3)) }
     end
 
-    context "when array elements are not distinct" do
-      let(:arr) { [10, 4, 90, 50, 23, 90] }
-      it { expect(subject).to_not eq(arr.max(3)) }
+    context "when the array contains duplicate elements" do
+      let(:arr) { [10, -4, 23, -50, 23, -90] }
+      it { expect(result).to eq(arr.uniq.max(3)) }
+    end
+
+    context "when array elements are negative" do
+      let(:arr) { [-10, -4, -3, -50, -23, -90] }
+      it { expect(result).to eq(arr.max(3)) }
+    end
+
+    context "when array elements are negative and positive" do
+      let(:arr) { [10, -4, 3, -50, 23, -90] }
+      it { expect(result).to eq(arr.max(3)) }
+    end
+
+    context "when array has exactly three distinct elements" do
+      let(:arr) { [30, 10, 20] }
+      it { expect(result).to eq([30, 20, 10]) }
+    end
+
+    context "when array is empty" do
+      let(:arr) { [] }
+      it { expect(result).to eq([]) }
     end
   end
 end
