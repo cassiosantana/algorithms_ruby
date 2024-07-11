c1c6050d8c6de6df259fdeb88d663a9bd115504d
Cassio Santana <cassioo.roger@gmail.com>
2024-07-11 15:23:00 -0300

    refactor: [wip] find three algorithm
    RF #50

--- lib/algorithms_ruby/find_three.rb
+++ lib/algorithms_ruby/find_three.rb
@@ -1,22 +1,50 @@
 # frozen_string_literal: true
 
-module Arrays
-  class FindTree
-    def self.find(arr)
-      first = second = third = 0
-      arr.size.times do |i|
-        if arr[i] > first
-          third = second
-          second = first
-          first = arr[i]
-        elsif (arr[i] > second) && (arr[i] != first)
-          third = second
-          second = arr[i]
-        elsif (arr[i] > third) && arr[i]
-          third = arr[i]
-        end
+module AlgorithmsRuby
+  class FindThree < Base
+    def initialize(arr)
+      @arr = arr
+      @first = @second = @third = -Float::INFINITY
+      super
+    end
+
+    def call
+      return [] if @arr.empty?
+
+      @arr.uniq.each do |element|
+        find_first(element)
+        find_second(element)
+        find_third(element)
       end
-      [first, second, third]
+
+      result
+    end
+
+    private
+
+    def find_first(element)
+      return unless element > @first
+
+      @third = @second
+      @second = @first
+      @first = element
+    end
+
+    def find_second(element)
+      return unless element > @second && element != @first
+
+      @third = @second
+      @second = element
+    end
+
+    def find_third(element)
+      return unless element > @third && element != @first && element != @second
+
+      @third = element
+    end
+
+    def result
+      [@first, @second, @third]
     end
   end
 end
