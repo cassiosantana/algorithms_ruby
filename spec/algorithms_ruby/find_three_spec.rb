70f7f4c0e2a5f2e50d6365ce1dbab691e8d3e9e9
Cassio Santana <cassioo.roger@gmail.com>
2024-07-12 12:43:33 -0300

    test: refactor find three test
    RF #50

--- spec/algorithms_ruby/find_three_spec.rb
+++ spec/algorithms_ruby/find_three_spec.rb
@@ -9,11 +9,6 @@ RSpec.describe AlgorithmsRuby::FindThree do
       it { expect(result).to eq(arr.max(3)) }
     end
 
-    context "when the array contains duplicate elements" do
-      let(:arr) { [10, -4, 23, -50, 23, -90] }
-      it { expect(result).to eq(arr.uniq.max(3)) }
-    end
-
     context "when array elements are negative" do
       let(:arr) { [-10, -4, -3, -50, -23, -90] }
       it { expect(result).to eq(arr.max(3)) }
@@ -22,16 +17,6 @@ RSpec.describe AlgorithmsRuby::FindThree do
     context "when array elements are negative and positive" do
       let(:arr) { [10, -4, 3, -50, 23, -90] }
       it { expect(result).to eq(arr.max(3)) }
-    end
-
-    context "when array has exactly three distinct elements" do
-      let(:arr) { [30, 10, 20] }
-      it { expect(result).to eq([30, 20, 10]) }
-    end
-
-    context "when array is empty" do
-      let(:arr) { [] }
-      it { expect(result).to eq([]) }
     end
   end
 end
