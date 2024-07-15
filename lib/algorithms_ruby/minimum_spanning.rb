051ebb5a66bc094b7e620f8a041c89895db469f2
Cassio Santana <cassioo.roger@gmail.com>
2024-07-14 23:53:51 -0300

    chore: add initial files for Minimum Spanning Tree algorithm
    Rf #5

---
+++ lib/algorithms_ruby/minimum_spanning.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Implementation of the algorithm found in:
+# https://www.geeksforgeeks.org/problem-solving-minimum-spanning-trees-kruskals-prims/
+
+module AlgorithmsRuby
+  class MinimumSpanning < Base
+
+  end
+end
